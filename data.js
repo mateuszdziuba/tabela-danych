b9758f568198ee387eefe851ba92b7a6ca17b65f
mateuszdziuba <mkdziuba@gmail.com>
2018-04-20 00:24:19 +0200

    Deploying

---
+++ data.js
@@ -0,0 +1,172 @@
+const users = [
+  {    name: "LeBron",
+    surname: "James",
+    age: 33
+  },
+  {    name: "Kevin",
+    surname: "Durant",
+    age: 29
+  },
+  {    name: "Stephen",
+    surname: "Curry",
+    age: 30
+  },
+  {    name: "Kawhi",
+    surname: "Leonard",
+    age: 26
+  },
+  {    name: "James",
+    surname: "Harden",
+    age: 28
+  },
+  {    name: "Russell",
+    surname: "Westbrook",
+    age: 29
+  },
+  {    name: "Chris",
+    surname: "Paul",
+    age: 32
+  },
+  {    name: "Anthony",
+    surname: "Davis",
+    age: 25
+  },
+  {    name: "Giannis",
+    surname: "Antetokounmpo",
+    age: 23
+  },
+  {
+    name: "Draymond",
+    surname: "Green",
+    age: 28
+  },
+  {
+    name: "Jimmy",
+    surname: "Butler",
+    age: 28
+  },
+  {
+    name: "Paul",
+    surname: "George",
+    age: 27
+  },
+  {
+    name: "John",
+    surname: "Wall",
+    age: 27
+  },
+  {
+    name: "Karl-Anthony",
+    surname: "Towns",
+    age: 22
+  },
+  {
+    name: "Rudy",
+    surname: "Gobert",
+    age: 25
+  },
+  {
+    name: "Gordon",
+    surname: "Hayward",
+    age: 28
+  },
+  {
+    name: "Damian",
+    surname: "Lillard",
+    age: 27
+  },
+  {
+    name: "Mike",
+    surname: "Conley",
+    age: 30
+  },
+  {
+    name: "Kyle",
+    surname: "Lowry",
+    age: 32
+  },
+  {
+    name: "Klay",
+    surname: "Thompson",
+    age: 28
+  },
+  {
+    name: "Kyrie",
+    surname: "Irving",
+    age: 26
+  },
+  {
+    name: "Blake",
+    surname: "Griffin",
+    age: 29
+  },
+  {
+    name: "DeMarcus",
+    surname: "Cousins",
+    age: 27
+  },
+  {
+    name: "Marc",
+    surname: "Gasol",
+    age: 33
+  },
+  {
+    name: "Nikola",
+    surname: "Jokic",
+    age: 23
+  },
+  {
+    name: "Kevin",
+    surname: "Love",
+    age: 29
+  },
+  {
+    name: "Paul",
+    surname: "Millsap",
+    age: 33
+  },
+  {
+    name: "DeAndre",
+    surname: "Jordan",
+    age: 29
+  },
+  {
+    name: "Kemba",
+    surname: "Walker",
+    age: 27
+  },
+  {
+    name: "Al",
+    surname: "Horford",
+    age: 31
+  },
+  {
+    name: "LaMarcus",
+    surname: "Aldridge",
+    age: 32
+  },
+  {
+    name: "Bradley",
+    surname: "Beal",
+    age: 24
+  },
+  {
+    name: "Kristaps",
+    surname: "Porzingis",
+    age: 22
+  },
+  {
+    name: "DeMar",
+    surname: "DeRozan",
+    age: 28
+  },
+  {
+    name: "Khris",
+    surname: "Middleton",
+    age: 26
+  }
+];
+
+module.exports = {
+  users: users
+};
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require('path')
+
+const data = require("./data");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get("/api/", (req, res) => {
+  const postCount = data.users.length;
+  const perPage = 10;
+  let page = 1;
+  const pageCount = Math.ceil(postCount / perPage);
+
+  res.json({
+    data: data.users.slice(0, perPage),
+    page: 1,
+    pageCount
+  });
+});
+
+app.post("/api/", (req, res) => {
+  const {
+    head,
+    order,
+    searchfield,
+    currentPage
+  } = req.body;
+
+  const filteredUsers = data.users.filter(user => {
+    return JSON.stringify(user)
+      .toLowerCase()
+      .includes(searchfield.toLowerCase());
+  });
+  const copy = [...filteredUsers];
+
+  const postCount = copy.length;
+  const perPage = 10;
+  let page = currentPage || 1;
+  const pageCount = Math.ceil(postCount / perPage);
+
+  let first = parseInt();
+  if (postCount <= 10) {
+    first = 0;
+  } else {
+    first = (page - 1) * perPage;
+  }
+
+  let second = page * perPage;
+
+  const compareValues = (key, order = "asc") => {
+    return (a, b) => {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        return 0;
+      }
+
+      const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return order === "desc" ? comparison * -1 : comparison;
+    };
+  };
+
+  res.json({
+    data: copy.sort(compareValues(head, order)).slice(first, second),
+    page,
+    pageCount
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+})
